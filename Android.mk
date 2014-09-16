8bd04f56c64cd4c46e8fa3e76c33331d3e371886
Olivier Sohn <olivier.sohn@gmail.com>
2014-09-16 23:40:49 +0200

    android makefile : adapt source declaration to allow ndk-build to correctly tag them as debug / release

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE    := os.storage
 
 #sources
 LOCAL_C_INCLUDES := include/
-LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/source/*.cpp)
+LOCAL_SRC_FILES := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/source/*.cpp))
 
 #build static library
 include $(BUILD_STATIC_LIBRARY)
