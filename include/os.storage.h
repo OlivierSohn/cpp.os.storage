3968230ff117b59bf97dc3af4d21d8af4401fe6a
Olivier Sohn <olivier.sohn@gmail.com>
2014-11-07 12:40:59 +0100

    calibration persistence

--- include/os.storage.h
+++ include/os.storage.h
@@ -15,39 +15,6 @@ enum eResult
 
 #define SIZE_READ_BUFFER 2048
 
-// data types
-
-#define DATA_TYPE_INT32        'a'
-#define DATA_TYPE_BOOL         'b'
-#define DATA_TYPE_DOUBLE       'd'
-#define DATA_TYPE_DOUBLE_ARRAY 'D'
-#define DATA_TYPE_FLOAT        'f'
-#define DATA_TYPE_FLOAT_ARRAY  'F'
-#define DATA_TYPE_CHAR         's'
-#define DATA_TYPE_CHAR_ARRAY   'S'
-
-// keys
-
-#define KEY_IC_DQROT     'a' // 4 double
-#define KEY_IC_DPOS      'b' // 3 double
-#define KEY_IC_SET       'c' // bool
-#define KEY_DATE_CREA    'd' // n * char
-#define KEY_GUID         'e' // n * char
-#define KEY_RAWPATH_GUID 'f' // n * char
-#define KEY_INTPATH_GUID 'g' // n * char
-#define KEY_REGPATH_GUID 'h' // n * char
-#define KEY_NAME         'i' // n * char
-#define KEY_ITGR_MODE    'j' // int32
-#define KEY_FILTER_RATE  'k' // double
-#define KEY_FILTER_CUTOFF 'l' // double
-#define KEY_FILTER_ADAPT 'm' // bool
-#define KEY_FPS          'n' // int32
-#define KEY_VEC_ACC_POS  'P' // n * 4 double
-#define KEY_VEC_INT_POS  'p' // n * 7 double
-#define KEY_VEC_ACC_ROT  'R' // n * 4 double
-#define KEY_VEC_INT_ROT  'r' // n * 5 double
-
-
 class Storage
 {
 public:
--- include/os.storage.keys.h
+++ include/os.storage.keys.h
@@ -1,7 +1,43 @@
-
 #pragma once
 #include "os.storage.h"
 #include "os.log.h"
+
+
+// data types
+
+#define DATA_TYPE_INT32        'a'
+#define DATA_TYPE_BOOL         'b'
+#define DATA_TYPE_DOUBLE       'd'
+#define DATA_TYPE_DOUBLE_ARRAY 'D'
+#define DATA_TYPE_FLOAT        'f'
+#define DATA_TYPE_FLOAT_ARRAY  'F'
+#define DATA_TYPE_CHAR         's'
+#define DATA_TYPE_CHAR_ARRAY   'S'
+
+// keys
+
+#define KEY_IC_DQROT            'a' // 4 double
+#define KEY_IC_DPOS             'b' // 3 double
+#define KEY_IC_SET              'c' // bool
+#define KEY_DATE_CREA           'd' // n * char
+#define KEY_GUID                'e' // n * char
+#define KEY_RAWPATH_GUID        'f' // n * char
+#define KEY_INTPATH_GUID        'g' // n * char
+#define KEY_REGPATH_GUID        'h' // n * char
+#define KEY_NAME                'i' // n * char
+#define KEY_ITGR_MODE           'j' // int32
+#define KEY_FILTER_RATE         'k' // double
+#define KEY_FILTER_CUTOFF       'l' // double
+#define KEY_FILTER_ADAPT        'm' // bool
+#define KEY_FPS                 'n' // int32
+#define KEY_VEC_ACC_POS         'P' // n * 4 double
+#define KEY_VEC_INT_POS         'p' // n * 7 double
+#define KEY_VEC_ACC_ROT         'R' // n * 4 double
+#define KEY_VEC_INT_ROT         'r' // n * 5 double
+#define KEY_ACC_SEN_BIAS        's' // 3 double
+#define KEY_ACC_SEN_MAX_GRAV    't' // 3 double
+#define KEY_ACC_SEN_MIN_GRAV    'u' // 3 double
+#define KEY_ACC_SEN_NOISE_AMP   'v' // 3 double
 
 class KeysPersist : public Storage
 {
