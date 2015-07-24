bf6e135d1933738ece1f9a3221e8fc3f7c176121
Olivier Sohn <olivier.sohn@gmail.com>
2015-07-24 03:58:46 -0400

    asynchronous directory operation, os abstraction

---
+++ include/os.abstraction.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace imajuscule
+{
+    class OSAbstraction
+    {
+    public:
+        OSAbstraction();
+        
+        virtual ~OSAbstraction();
+        
+        static OSAbstraction * get();
+        
+        void registerAbstraction(OSAbstraction *);
+        void unregisterAbstraction(OSAbstraction *);
+
+        virtual void PauseInteractions(bool) = 0;
+
+    private:
+        static OSAbstraction * m_registered;
+    };
+}
---
+++ source/os.abstraction.cpp
@@ -0,0 +1,32 @@
+#include <cstddef> // NULL
+#include "os.abstraction.h"
+#include "os.log.h"
+
+using namespace imajuscule;
+OSAbstraction * OSAbstraction::m_registered = NULL;
+
+OSAbstraction * OSAbstraction::get()
+{
+    A(m_registered);
+    return m_registered;
+}
+
+OSAbstraction::OSAbstraction()
+{
+    
+}
+OSAbstraction::~OSAbstraction()
+{
+    
+}
+
+void OSAbstraction::registerAbstraction(OSAbstraction * a)
+{
+    A(!m_registered);
+    m_registered = a;
+}
+void OSAbstraction::unregisterAbstraction(OSAbstraction * a)
+{
+    A(m_registered == a);
+    m_registered = NULL;
+}
