b56c31f13c33146f6834b84de823dbf43e23480b
Olivier Sohn <olivier.sohn@gmail.com>
2016-09-19 18:07:17 -0400

    linux

--- source/Posix.cpp
+++ source/Posix.cpp
@@ -1,14 +1,25 @@
-#import <Foundation/Foundation.h>
+#if __APPLE__
+# import <Foundation/Foundation.h>
+#else
+# include <unistd.h>
+# include <sys/types.h>
+# include <pwd.h>
+#endif
 
 #include "platform.h"
 
 namespace imajuscule {
     
     DirectoryPath Posix::user_path() {
-        char path[1024];
 #if __APPLE__
+        char path[1024];
         CFStringGetCString( (CFStringRef)NSHomeDirectory() , path , sizeof(path) , kCFStringEncodingUTF8 );
 #else
+        const char *path;
+
+        if ((path = getenv("HOME")) == NULL) {
+            path = getpwuid(getuid())->pw_dir;
+        }
 #endif
         return DirectoryPath(path);
     }
