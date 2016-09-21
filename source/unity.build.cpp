321f01134dc1ba4f007b6413cac89c0a37b35e3b
Olivier Sohn <olivier.sohn@gmail.com>
2016-09-20 22:27:41 -0400

    unity

--- source/unity.build.cpp
+++ source/unity.build.cpp
@@ -1,9 +1,18 @@
+#if _WIN32
+# include "Windows.cpp"
+#elif __linux__
+# include "Posix.cpp"
+# include "linux/LinuxDialogs.cpp"
+#endif
+
+#ifndef __APPLE__
+# include  "read.resources.cpp"
+#endif
+
 #include "file2string.cpp"
 #include "os.abstraction.cpp"
 #include "os.file.dialog.cpp"
 #include "os.storage.cpp"
 #include "os.storage.keys.cpp"
 #include "win32/Win32Dialogs.cpp"
-#ifdef WIN32
-#include "Windows.cpp"
-#endif
+
