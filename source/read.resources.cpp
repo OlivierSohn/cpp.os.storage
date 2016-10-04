d45076e6ba7670be7237eb7fb400fcfa4c21687d
Olivier Sohn <olivier.sohn@gmail.com>
2016-10-04 01:21:15 -0400

    removed (a lot of) delete's

--- source/read.resources.cpp
+++ source/read.resources.cpp
@@ -22,10 +22,9 @@ namespace imajuscule {
         int len;
         int slength = (int)s.length() + 1;
         len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
-        wchar_t* buf = new wchar_t[len];
-        MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
-        std::wstring r(buf);
-        delete[] buf;
+        std::unique_ptr<wchar_t[]> buf(new wchar_t[len]);
+        MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf.get(), len);
+        std::wstring r(buf.get());
         return r;
     }
     std::string GetLastErrorAsString()
