4f682a9cf5a8bf3be9d33f51e210c771ab142bcd
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-09 22:04:32 +0200

    Windows compilation

--- source/private.h
+++ source/private.h
@@ -5,10 +5,20 @@
 #  ifndef NOMINMAX
 #    define NOMINMAX
 #  endif
-#  include <windows.h>
-#  include "Shlobj.h"
+#  include <windows.h>      // For common windows data types and function headers
+#  define STRICT_TYPED_ITEMIDS
+#  include <shlobj.h>
+#  include <objbase.h>      // For COM headers
+#  include <shobjidl.h>     // for IFileDialogEvents and IFileDialogControlEvents
+#  include <shlwapi.h>
+#  include <knownfolders.h> // for KnownFolder APIs/datatypes/function headers
+#  include <propvarutil.h>  // for PROPVAR-related functions
+#  include <propkey.h>      // for the Property key APIs/datatypes
+#  include <propidl.h>      // for the Property System APIs
+#  include <strsafe.h>      // for StringCchPrintfW
+#  include <shtypes.h>      // for COMDLG_FILTERSPEC
 #  include <tchar.h>
-#  include <strsafe.h>
+#  pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #else
 #  include <dirent.h>
 #  include <sys/types.h>
--- source/win32/Win32Dialogs.cpp
+++ source/win32/Win32Dialogs.cpp
@@ -9,22 +9,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //
 
-#include <windows.h>      // For common windows data types and function headers
-#define STRICT_TYPED_ITEMIDS
-#include <shlobj.h>
-#include <objbase.h>      // For COM headers
-#include <shobjidl.h>     // for IFileDialogEvents and IFileDialogControlEvents
-#include <shlwapi.h>
-#include <knownfolders.h> // for KnownFolder APIs/datatypes/function headers
-#include <propvarutil.h>  // for PROPVAR-related functions
-#include <propkey.h>      // for the Property key APIs/datatypes
-#include <propidl.h>      // for the Property System APIs
-#include <strsafe.h>      // for StringCchPrintfW
-#include <shtypes.h>      // for COMDLG_FILTERSPEC
-#include <new>
-
-#pragma comment(linker, "\"/manifestdependency:type='Win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 // Indices of file types
 #define INDEX_WORDDOC 1
 #define INDEX_WEBPAGE 2
